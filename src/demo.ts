de8ea58af3e155f616829a1460fd0053414f4649
Issmailduman <https://github.com/Issmailduman>
2023-01-17 12:39:58 +0100

    user0

--- src/demo.ts
+++ src/demo.ts
@@ -42,3 +42,22 @@ const personx = new Personx('scott', 'rose');
 const person2 = new Personx('adam', 'johnson');
 console.log(personx);
 console.log(person2);
+
+class User0{
+    name:string;
+    email:string;
+    age:number;
+
+
+    constructor(name:string,email:string,age:number){
+        this.age=age;
+        this.email=email;
+        this.name=name;
+
+    }
+}
+
+class AdminUser1 extends User0{
+isAdmin:boolean=true;
+
+}
